c8abd382b569257dff3ff5bceb7d8e391395214f
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-29 03:26:29 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -19,7 +19,7 @@ class Boat < ActiveRecord::Base
     all.order(name: :desc).limit(3)
   end
   
-  def self.without_a_capitain
+  def self.without_a_captain
     where('captain_id: nil')
   end
   
